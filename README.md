28c1f773f5a71bc3d48b25acdba0a2f774dfbdb3
Jebbee <jeffreywjohnson+git@gmail.com>
2017-04-15 19:59:26 -0700

    Wired up to do actual copy. Maven one-jar

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # minecraft-backup
-Used to backup minecraft world saves
+
+Used to backup minecraft world saves.
+
+## TODO
+
+* [ ] Improve UI layout
+* [ ] Verify that backup was successful
+* [ ] Get default properties (directory paths) from properties file (~/.minecraft-backup/minecraft-backup.properties)
+* [ ] Optionally purge older backups (to save disk space)
+* [ ] As native app
+    * http://docs.oracle.com/javase/7/docs/technotes/guides/jweb/packagingAppsForMac.html
+    * http://alvinalexander.com/apple/mac/java-jar-bundler/
--- src/main/groovy/MinecraftBackup.groovy
+++ src/main/groovy/MinecraftBackup.groovy
@@ -1,6 +1,10 @@
+//@groovy.lang.Grab(group = 'commons-io', module = 'commons-io', version = '2.5')
+
+
 import groovy.io.FileType
 import groovy.swing.SwingBuilder
 import groovy.transform.Field
+import org.apache.commons.io.FileUtils
 
 import javax.swing.*
 
@@ -42,7 +46,7 @@ new SwingBuilder().edt {
                         weightx: 0,
                         weighty: 0
                 ),
-                text: 'Saves Directory:'
+                text: 'Saves directory:'
         )
         JTextField minecraftSavesDirTextField = textField(
                 constraints: gbc(
@@ -86,10 +90,24 @@ new SwingBuilder().edt {
                     }
                 }
         )
-        scrollPane(
+        label(
                 constraints: gbc(
                         gridx: 0,
                         gridy: 1,
+                        gridwidth: 1,
+                        gridheight: 1,
+                        fill: NONE,
+                        anchor: WEST,
+                        insets: [5, 5, 0, 0],
+                        weightx: 0,
+                        weighty: 0
+                ),
+                text: 'Select worlds to backup:'
+        )
+        scrollPane(
+                constraints: gbc(
+                        gridx: 0,
+                        gridy: 2,
                         gridwidth: REMAINDER,
                         gridheight: 1,
                         fill: BOTH,
@@ -106,7 +124,7 @@ new SwingBuilder().edt {
         label(
                 constraints: gbc(
                         gridx: 0,
-                        gridy: 2,
+                        gridy: 3,
                         gridwidth: 1,
                         gridheight: 1,
                         fill: NONE,
@@ -115,12 +133,12 @@ new SwingBuilder().edt {
                         weightx: 0,
                         weighty: 0
                 ),
-                text: 'Backup Directory:'
+                text: 'Backup directory:'
         )
         JTextField backupSavesDirTextField = textField(
                 constraints: gbc(
                         gridx: 1,
-                        gridy: 2,
+                        gridy: 3,
                         gridwidth: 1,
                         gridheight: 1,
                         fill: HORIZONTAL,
@@ -134,7 +152,7 @@ new SwingBuilder().edt {
         button(
                 constraints: gbc(
                         gridx: 2,
-                        gridy: 2,
+                        gridy: 3,
                         gridwidth: REMAINDER,
                         gridheight: 1,
                         fill: NONE,
@@ -161,7 +179,7 @@ new SwingBuilder().edt {
         button(
                 constraints: gbc(
                         gridx: 0,
-                        gridy: 3,
+                        gridy: 4,
                         gridwidth: REMAINDER,
                         gridheight: 1,
                         fill: NONE,
@@ -181,16 +199,12 @@ new SwingBuilder().edt {
 def populateMinecraftSavesDirectoryNamesListModel() {
     minecraftSavesDirectoryNamesListModel.clear()
     println "minecraftSavesDir = $minecraftSavesDir"
-    minecraftSavesDir.eachFile(FileType.DIRECTORIES) { directory ->
+    minecraftSavesDir.eachFile(FileType.DIRECTORIES) { File directory ->
         minecraftSavesDirectoryNamesListModel.addElement(directory.name)
     }
 }
 
 def backup(List<String> directoryNamesToBackup) {
-    if (!backupSavesDir.exists() && !backupSavesDir.mkdirs()) {
-        throw new RuntimeException("Unable to create directory $backupSavesDir")
-    }
-
     directoryNamesToBackup.each { String sourceDirectoryToBackupName ->
         File sourceDirectoryToBackup = new File(minecraftSavesDir as File, sourceDirectoryToBackupName)
         File targetDirectoryToBackupTo = new File(
@@ -199,7 +213,10 @@ def backup(List<String> directoryNamesToBackup) {
         )
 
         println "Copy from $sourceDirectoryToBackup to $targetDirectoryToBackupTo"
-        // TODO Copy the directory
-        // See http://tutorials.jenkov.com/java-nio/files.html#overwriting-existing-files
+        FileUtils.copyDirectory(sourceDirectoryToBackup, targetDirectoryToBackupTo)
     }
+    println 'Backup complete!'
+
+    // TODO (low) Add minecraft icon to message dialog
+    JOptionPane.showMessageDialog(mainFrame, 'Backup complete!', 'Minecraft World Backup', JOptionPane.INFORMATION_MESSAGE)
 }
